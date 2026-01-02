6afef75727b9f7929aacce3203e7a076e0498678
McNugget <alexd84a@gmail.com>
2026-01-02 20:50:40 +0100

    final commit

---
+++ Template/Template.SourceGenerator/FartMangler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace Template.SourceGenerator
+{
+    [Generator]
+    public class FartMangler : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            IncrementalValuesProvider<string> files = context.AdditionalTextsProvider
+                .Where(file => file.Path.EndsWith("i_hate_dolphins.txt"))
+                .Select((file, text) => file.GetText(text)?.ToString());
+
+            context.RegisterSourceOutput(files, (sourceProductionContext, content) =>
+            {
+                if (string.IsNullOrEmpty(content))
+                    return;
+
+                string generatedFile = $"// ----- Generated\r\n\r\nnamespace Template;\r\n\r\ninternal static class Hello\r\n{{\r\n\tpublic static readonly string ItsMe = \"{content}\";\r\n}}";
+                sourceProductionContext.AddSource("Kawaii.g.cs", generatedFile);
+            });
+        }
+    }
+}
---
+++ Template/Template/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.Windows.ApplicationModel.DynamicDependency;
+using System;
+using System.Threading;
+
+namespace Template;
+
+internal class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        _ = args;
+
+        Bootstrap.Initialize(0x00010008);
+
+        try
+        {
+            Application.Start((param) =>
+            {
+                DispatcherQueueSynchronizationContext context = new(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                _ = new App();
+            });
+        }
+        finally
+        {
+            Bootstrap.Shutdown();
+        }
+    }
+}
